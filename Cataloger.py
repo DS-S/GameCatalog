8f0afd258fd535f81c48dc497d6cc92fa5e86415
Dennis Santos-Sanchez <dennis5santos@live.com>
2021-03-28 23:08:22 -0400

    Addition of interpret() function and minor changes to main.

--- Cataloger.py
+++ Cataloger.py
@@ -2,7 +2,26 @@
 Main Program
 Author: Dennis Santos-Sanchez
 """
+import sqlalchemy as al
+import sqlite3 as lt
+
+def interpret(cmd):
+    while cmd != "":
+        if cmd =="New":
+        if cmd =="Load":
+    return
+
 def main():
+    print("Welcome to the GameCataloger!")
+    print("If you would like to start a new catalog enter the command: New") #Fill in <Command>
+    print("If you would like to use an existing catalog enter the command: Load") #Fill in <Command>
+    #create Title Table
+    #create Played Table
+    #create completed Table
+    #create Genre Table
+    #create Release Date Table
+    #create Systems Table
+    cmd = input("Enter Command:")
 
 if __name__ == "__main__":
     main()
