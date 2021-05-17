bd5090688b1d6e626c9d411544da5d6010585800
Dennis Santos-Sanchez <drs2559@rit.edu>
2021-05-17 14:20:47 -0400

    Restructuring

--- Cataloger.py
+++ Cataloger.py
@@ -2,38 +2,108 @@
 Main Program
 Author: Dennis Santos-Sanchez
 """
-import sqlalchemy
-import sqlite3
 import os
+import sqlite3
+
+import sqlalchemy
+from sqlalchemy import create_engine, Column, Integer, String, Boolean, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
 
 def new_catalog():
+    # Get path
     path = input("Please enter the path to the directory where you would like the catalog to be stored\n"
-                 "This includes the drive letter (e.g. C:\\user\\<user>\\Documents):")
-    if(not os.path.exists(path)):
-        print("Path does not exist, please enter existing path.")
+                 "This includes the drive letter (e.g. C:\\Users\\<user>\\Documents):")
+
+    # Checks path exists.
+    if not os.path.exists(path):
+        print("Path does not exist, please enter existing path.")  # Must be better way than to return
         return None
+
+    # Get file name
     file = input("Please enter the catalog file name:")
+
     filepath = path + "\\" + file
-    # create GameTable
-    # create Genre Table
-    # create Platforms Table
+
+    # Checks file doesn't already exists
+    if not os.path.isfile(filepath):
+        print(
+            "File does already exits in directory, please enter non-existing filename.")  # Must be better way than to return
+        return None
+
+    #### Building Tables ####
+    Base = declarative_base()
+
+    class Game(Base):
+        __tablename__ = "game"
+
+        id = Column(Integer, primary_key=True)
+        title = Column(String, nullable=False)
+        played = Column(Boolean, nullable=False)
+        completed = Column(Boolean, nullable=False)
+        # relationship (<Class related to>, secondary=<Table that forms relation between 2 other tables>,
+        # back_populates=<complimentary collection in related class>)
+        platforms = relationship("Platform", secondary="Game_Platform_link", back_populates="games")
+        genres = relationship("Genre", secondary="Game_Genre_link", back_populates="games")
+
+    class Platform(Base):
+        __tablename__ = "platform"
+
+        id = Column(Integer, primary_key=True, nullable=False)
+        platform_name = Column(String, nullable=False)
+        # relationship (<Class related to>, secondary=<Table that forms relation between 2 other tables>,
+        # back_populates=<complimentary collection in related class>)
+        games = relationship("Game", secondary="Game_Platform_link", back_populates="platforms")
+
+    class Genre(Base):
+        __tablename__ = "genre"
+
+        id = Column(Integer, primary_key=True, nullable=False)
+        platform_name = Column(String, nullable=False)
+        # relationship (<Class related to>, secondary=<Table that forms relation between 2 other tables>,
+        # back_populates=<complimentary collection in related class>)
+        games = relationship("Game", secondary="Game_Genre_link", back_populates="genres")
+
+    class GamePlatform(Base):
+        __tablename__ = "Game_Platform_link"
+
+        game_id = Column(Integer, ForeignKey("game.id"), primary_key=True)
+        platform_id = Column(Integer, ForeignKey("platform.id"), primary_key=True)
+
+    class GameGenre(Base):
+        __tablename__ = "Game_Genre_link"
+
+        game_id = Column(Integer, ForeignKey("game.id"), primary_key=True)
+        platform_id = Column(Integer, ForeignKey("genre.id"), primary_key=True)
+
+    # Create engine object that acts as central source of connections to the database
+    # Creates a new database as the file doesn't exist
+    engine = create_engine("sqlite+pysqlite:///" + filepath, echo=False, future=True)
+
+    #### Create all tables in the engine/database ####
+    Base.metadata.create_all(engine)
+
+    # with Session(engine) as session: Seesion?
 
     return
+
 
 def load_catalog():
     path = input("Please enter the path to the directory where the catalog is stored:")
     if not os.path.exists(path):
-        print("Path does not exist, please enter existing path.")
+        print("Path does not exist, please enter existing path.")  # Must be better way than to return
         return None
     file = input("Please enter the catalog file name:")
     filepath = path + "\\" + file
     if not os.path.isfile(filepath):
-        print("File does not exits in directory, please enter existing file.")
+        print("File does not exits in directory, please enter existing file.")  # Must be better way than to return
         return None
 
+
 def main():
+    print("SQLAlchemy: " + sqlalchemy.__version__ + "\nSQLite3: " + sqlite3.version)
     print("Welcome to the GameCataloger!")
-    print(sqlalchemy.__version__+"\n"+sqlite3.version)
     print("If you would like to start a new catalog enter the command: New")
     print("If you would like to use an existing catalog enter the command: Load")
     cmd = input("Enter Command:")
@@ -43,6 +113,7 @@ def main():
     if cmd == "Load":
         load_catalog()
         return
+
 
 if __name__ == "__main__":
     main()
