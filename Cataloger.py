0041d666e18f9c75a1ef1d2d275dfd308f395464
Dennis Santos-Sanchez <dennis5santos@live.com>
2021-07-19 10:28:15 -0400

    Added check for bad filename. Adjusted existing path and file checks. Adjusted Printing of text to be more easily read.

--- Cataloger.py
+++ Cataloger.py
@@ -17,22 +17,26 @@ Creates and connects to new database
 
 def new_catalog():
     # Get path
-    path = input("Please enter the path to the directory where you would like the catalog to be stored\n"
+    path = input("\nPlease enter the path to the directory where you would like the catalog to be stored\n"
                  "This includes the drive letter (e.g. C:\\Users\\<user>\\Documents):")
     # Checks path exists.
     if not os.path.exists(path):
-        print("Path does not exist, please enter existing path.")  # Must be better way than to return, how ask again?
-        return None
+        print("\nPath does not exist, please enter existing path.")  # Must be better way than to return, how ask again?
+        return False
     # Get file name
-    file = input("Please enter the catalog file name:")
-    # ToDo: Insert check for special characters in file name. Can't use them
+    file = input("\nPlease enter the catalog file name:")
+    invalidChars = "\/:*?<>| "
+    for char in file:
+        if char in invalidChars:
+            print("\nBad file name.")
+            return False
     filepath = path + "\\" + file
     # Checks file doesn't already exists
-    if not os.path.isfile(filepath):
+    if os.path.isfile(filepath):
         print(
-            "File already exits in directory, please enter non-existing filename.")
+            "\nFile already exits in directory, please enter non-existing filename.")
         # Must be better way than to return, how ask again?
-        return None
+        return False
 
     ### Create Engine ###
     # Create engine object that acts as central source of connections to the database
@@ -97,51 +101,75 @@ Connects to existing database
 
 
 def load_catalog():
-    path = input("Please enter the path to the directory where the catalog is stored:")
+    path = input("\nPlease enter the path to the directory where the catalog is stored:")
     if not os.path.exists(path):
-        print("Path does not exist, please enter existing path.")  # Must be better way than to return
-        return None
-    file = input("Please enter the catalog file name:")
+        print("\nPath does not exist, please enter existing path.")  # Must be better way than to return
+        return False
+    file = input("\nPlease enter the catalog file name:")
+    invalidChars = "\/:*?<>| "
+    for char in file:
+        if char in invalidChars:
+            print("\nBad file name.")
+            return False
     filepath = path + "\\" + file
     if not os.path.isfile(filepath):
         print(
-            "File does not exits in directory, please enter existing file or use the command to create a new file.")
+            "\nFile does not exits in directory, please enter existing file or use the command to create a new file.")
         # Must be better way than to return
-        return None
+        return False
 
 
 ### MENU SYSTEM ###
 
 def initial_menu():
-    print("To start a new log enter the command: New")
+    print("\nTo start a new log enter the command: New")
     print("To use an existing catalog enter the command: Load")
     print("To quit the program enter the command: Quit\n")
     cmd = input("Enter Command:")
     while cmd != "Quit":
         if cmd == "New":
-            new_catalog()
+            if new_catalog() == False:
+                print("\nTo start a new log enter the command: New")
+                print("To use an existing catalog enter the command: Load")
+                print("To quit the program enter the command: Quit\n")
+                cmd = input("Enter Command:")
+                continue
             # ToDo: display entire catalog
-            sub_menu()
-            return
+            else:
+                sub_menu()
+                print("\nTo start a new log enter the command: New")
+                print("To use an existing catalog enter the command: Load")
+                print("To quit the program enter the command: Quit\n")
+                cmd = input("Enter Command:")
         elif cmd == "Load":
-            load_catalog()
+            if load_catalog() == False:
+                print("\nTo start a new log enter the command: New")
+                print("To use an existing catalog enter the command: Load")
+                print("To quit the program enter the command: Quit\n")
+                cmd = input("Enter Command:")
+                continue
             # ToDo: display entire catalog
-            sub_menu()
-            return
+            else:
+                sub_menu()
+                print("\nTo start a new log enter the command: New")
+                print("To use an existing catalog enter the command: Load")
+                print("To quit the program enter the command: Quit\n")
+                cmd = input("Enter Command:")
         else:
             print("\nTo start a new log enter the command: New")
             print("To use an existing catalog enter the command: Load")
             print("To quit the program enter the command: Quit\n")
             cmd = input("Enter Command:")
+    return
 
 
 def sub_menu():
-    print("All cataloged games have been displayed above.\nYou are now in the sub-menu.")
-    print("To search for a game by title enter the command: Search")
+    print("\nAll cataloged games have been displayed above.\nYou are now in the sub-menu.")
+    print("\nTo search for a game by title enter the command: Search")
     print("To display the catalog using filters enter the command: Filter")
     print("Otherwise to return to the initial menu to create or load a different catalog enter the command: Exit")
-    cmd = input("Enter Command:")
-    while cmd != "Exit!":
+    cmd = input("\nEnter Command:")
+    while cmd != "Exit":
         if cmd == "Search":
             # ToDo: create database search function
             return
@@ -149,7 +177,7 @@ def sub_menu():
             # ToDo: create database filter function
             return
         else:
-            print("To search for a game by title enter the command: Search")
+            print("\nTo search for a game by title enter the command: Search")
             print("To display the catalog using filters enter the command: Filter")
             print("Otherwise to return to the initial menu to create or load a different catalog enter the command: "
                   "Exit")
@@ -159,7 +187,7 @@ def sub_menu():
 
 def main():
     print("SQLAlchemy: " + sqlalchemy.__version__ + "\nSQLite3: " + sqlite3.version)
-    print("Welcome to the GameTracker!\n")
+    print("Welcome to the GameTracker!")
     initial_menu()
 
 
