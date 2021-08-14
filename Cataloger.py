f4e08ad6efbc8f181daf685b798771da40f39443
Dennis Santos-Sanchez <dennis5santos@live.com>
2021-08-14 00:05:48 -0400

    Moved table clasees to global scope. Seperated new and old file path checking into separate functions. Creating/connecting to database and creating/loading table now in seperate function. Created function to get all games from database in alphabetical order and then used this function to display all games after making/loading a database in initial menu.

--- Cataloger.py
+++ Cataloger.py
@@ -3,136 +3,108 @@ Main Program
 Author: DS-S
 """
 import os
-import sqlite3
-
-import sqlalchemy
-from sqlalchemy import create_engine, Column, Integer, String, Boolean, ForeignKey, select
+from sqlalchemy import create_engine, Table, Column, Integer, String, Boolean, ForeignKey, select
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import Session, relationship
-from sqlalchemy.ext.automap import automap_base
 
-"""
-Creates New Database
-"""
-def new_catalog():
+Base = declarative_base()
+
+gplink = Table("game_platform_link", Base.metadata,
+               Column("game_id", ForeignKey("game.id"), primary_key=True),
+               Column("platform_id", Integer, ForeignKey("platform.id"), primary_key=True))
+
+gglink = Table("game_genre_link", Base.metadata,
+               Column("game_id", ForeignKey("game.id"), primary_key=True),
+               Column("genre_id", Integer, ForeignKey("genre.id"), primary_key=True))
+
+class Game(Base):
+    __tablename__ = "game"
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String, nullable=False)
+    played = Column(Boolean, nullable=False)
+    completed = Column(Boolean, nullable=False)
+    # relationship (<Class related to>, secondary=<Table that forms relation between 2 other tables>,
+    # back_populates=<complimentary collection in related class>)
+    platforms = relationship("Platform", secondary=gplink, back_populates="games")
+    genres = relationship("Genre", secondary=gglink, back_populates="games")
+
+
+class Platform(Base):
+    __tablename__ = "platform"
+
+    id = Column(Integer, primary_key=True)
+    platform_name = Column(String, nullable=False)
+    games = relationship("Game", secondary=gplink, back_populates="platforms")
+
+
+class Genre(Base):
+    __tablename__ = "genre"
+
+    id = Column(Integer, primary_key=True)
+    genre_name = Column(String, nullable=False)
+    games = relationship("Game", secondary=gglink, back_populates="genres")
+
+def connect(filepath):
+    # Create engine
+    engine = create_engine("sqlite+pysqlite:///" + filepath, echo=False, future=True)
+    # Create all tables in the database
+    Base.metadata.create_all(engine)
+    # Return engine to connect to database later
+    return engine
+
+def display_all(engine):
+    with Session(engine) as session:
+        return session.execute(select(Game.title, Game.played, Game.completed, Platform.platform_name, Genre.genre_name).join(
+            Game.platforms).join(Game.genres)).all()
+
+def new_path():
     # Get path
     path = input("\nPlease enter the path to the directory where you would like the catalog to be stored\n"
                  "This includes the drive letter (e.g. C:\\Users\\<user>\\Documents):")
-
     # Check path exists
     if not os.path.exists(path):
         print("\nPath does not exist, please enter existing path.")  # Must be better way than to return, how ask again?
         return
-
     # Get file name
     file = input("\nPlease enter the catalog file name:")
-
     # Check file name
     invalidChars = "\/:*?<>| "
     for char in file:
         if char in invalidChars:
             print("\nBad file name.")
             return
-
     filepath = path + "\\" + file
-
-    # Check file does not already exist
+    # Check that file does not exist
     if os.path.isfile(filepath):
         print(
             "\nFile already exits in directory, please enter non-existing filename.")
         return
+    return  filepath
 
-    # Create engine
-    engine = create_engine("sqlite+pysqlite:///" + filepath, echo=False, future=True)
-
-    # Build tables
-    Base = declarative_base()
-
-    class Game(Base):
-        __tablename__ = "game"
-
-        id = Column(Integer, primary_key=True)
-        title = Column(String, nullable=False)
-        played = Column(Boolean, nullable=False)
-        completed = Column(Boolean, nullable=False)
-        # relationship (<Class related to>, secondary=<Table that forms relation between 2 other tables>,
-        # back_populates=<complimentary collection in related class>)
-        platform = relationship("Platform", secondary="Game_Platform_link", back_populates="games")
-        genre = relationship("Genre", secondary="Game_Genre_link", back_populates="games")
-
-    class Platform(Base):
-        __tablename__ = "platform"
-
-        id = Column(Integer, primary_key=True, nullable=False)
-        platform_name = Column(String, nullable=False)
-        game = relationship("Game", secondary="Game_Platform_link", back_populates="platforms")
-
-    class Genre(Base):
-        __tablename__ = "genre"
-
-        id = Column(Integer, primary_key=True, nullable=False)
-        platform_name = Column(String, nullable=False)
-        game = relationship("Game", secondary="Game_Genre_link", back_populates="genres")
-
-    class GamePlatform(Base):
-        __tablename__ = "Game_Platform_link"
-
-        game_id = Column(Integer, ForeignKey("game.id"), primary_key=True)
-        platform_id = Column(Integer, ForeignKey("platform.id"), primary_key=True)
-
-    class GameGenre(Base):
-        __tablename__ = "Game_Genre_link"
-
-        game_id = Column(Integer, ForeignKey("game.id"), primary_key=True)
-        platform_id = Column(Integer, ForeignKey("genre.id"), primary_key=True)
-
-    # Create all tables in the database
-    Base.metadata.create_all(engine)
-
-    # Return engine to connect to database later
-    return engine
-
-
-"""
-Connects to existing database
-"""
-def load_catalog():
+def load_path():
+    # Get path
     path = input("\nPlease enter the path to the directory where the catalog is stored:")
-
+    # Check path exists
     if not os.path.exists(path):
         print("\nPath does not exist, please enter existing path.")
         return
-
+    # Get file name
     file = input("\nPlease enter the catalog file name:")
-
+    # Check file name
     invalidChars = "\/:*?<>| "
     for char in file:
         if char in invalidChars:
             print("\nBad file name.")
             return
-
+    # make file path
     filepath = path + "\\" + file
-
+    # Check that file exists
     if not os.path.isfile(filepath):
         print(
             "\nFile does not exits in directory, please enter existing file or use the command to create a new file.")
         return
-
-    engine = create_engine("sqlite+pysqlite:///" + filepath, echo=False, future=True)
-
-    # Use automap to copy tables from existing database
-    Base = automap_base()
-    Base.prepare(engine, reflect=True)
-
-    # Copy main tables
-    Game = Base.classess.game
-    Platform = Base.clasees.platform
-    Genre = Base.classes.genre
-    # Copy Association tables
-    GamePlatform = Base.classes.Game_Platform_link  # Unsure if should be table name or class name
-    GameGenre = Base.classes.Game_Genre_link
-
-    return engine
+    return filepath
 
 
 """
@@ -145,35 +117,40 @@ def initial_menu():
     cmd = input("Enter Command:")
     while cmd != "Quit":
         if cmd == "New":
-            engine = new_catalog()
-            if engine is None:
+            filepath = new_path()
+            if filepath is None:
                 print("\nTo start a new catalog enter the command: New")
                 print("To use an existing catalog enter the command: Load")
                 print("To quit the program enter the command: Quit\n")
                 cmd = input("Enter Command:")
                 continue
             else:
-                with Session(engine) as session:
-                    stmt= select(Game.title, Game.played, Game.completed, Platform, Genre).where(
-                        Game.id == Platform.game).where(Game.id == Genre.game).order_by(Game.title)
-                    result = session.execute(stmt).all()
-                    for row in result:
-                        print(row)
+                engine = connect(filepath)
+                all_games = display_all(engine)
+                print("")
+                for row in all_games:
+                    print(f"Title: {row.title} || Played: {row.played} || Completed: {row.completed} || "
+                          f"Platform: {row.platform_name} || Genre: {row.genre_name}")
                 sub_menu()
                 print("\nTo start a new catalog enter the command: New")
                 print("To use an existing catalog enter the command: Load")
                 print("To quit the program enter the command: Quit\n")
                 cmd = input("Enter Command:")
         elif cmd == "Load":
-            engine = load_catalog()
-            if engine is None:
+            filepath = load_path()
+            if filepath is None:
                 print("\nTo start a new catalog enter the command: New")
                 print("To use an existing catalog enter the command: Load")
                 print("To quit the program enter the command: Quit\n")
                 cmd = input("Enter Command:")
                 continue
             else:
-                # ToDo: display entire catalog
+                engine = connect(filepath)
+                all_games = display_all(engine)
+                print("")
+                for row in all_games:
+                    print(f"Title: {row.title} || Played: {row.played} || Completed: {row.completed} || "
+                          f"Platform: {row.platform_name} || Genre: {row.genre_name}")
                 sub_menu()
                 print("\nTo start a new catalog enter the command: New")
                 print("To use an existing catalog enter the command: Load")
@@ -223,7 +200,6 @@ def sub_menu():
 
 
 def main():
-    print("SQLAlchemy: " + sqlalchemy.__version__ + "\nSQLite3: " + sqlite3.version)
     print("Welcome to the GameTracker!")
     initial_menu()
 
